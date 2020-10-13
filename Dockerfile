910026b7a1424a06eb49db125dd9a2be9f182a93
root <root@chutoro.kondi.net>
2020-10-13 20:41:08 +0200

    work in progress

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ruby:2.7.1-slim
 
 ENV APPROOT /usr/src/app
 ENV NODE_MAJOR 12
-ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/opt/yarn/bin
+ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/src/app/bin:/opt/yarn/bin
 
 RUN apt-get update && \
     apt-get -y install imagemagick libpq-dev autoconf libtool gnupg curl git xz-utils libxml2 build-essential patch zlib1g-dev liblzma-dev && \
@@ -12,7 +12,7 @@ RUN apt-get update && \
 RUN bundle config --global frozen 1
 
 WORKDIR $APPROOT
-RUN useradd -d $APPROOT -s /bin/false app && chown -R app:app $APPROOT
+RUN useradd -d $APPROOT -s /bin/false app
 
 COPY containers/js_setup.sh /usr/bin/
 RUN bash /usr/bin/js_setup.sh
@@ -20,20 +20,21 @@ RUN bash /usr/bin/js_setup.sh
 COPY Gemfile Gemfile.lock ./
 RUN bundle install
 
+COPY containers/entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+
 #COPY --chown=app:app . .
 COPY . .
 RUN chown app:app -R $APPROOT && chmod 777 $APPROOT/tmp $APPROOT/log $APPROOT/storage
 
-RUN yarn install --network-timeout 1000000
+USER app
+RUN mkdir -p $APPROOT/.cache/yarn && \
+    yarn install --network-timeout 1000000 && \
+    webpack --verbose
 
 EXPOSE 3000
 
-COPY containers/entrypoint.sh /usr/bin/
-RUN chmod +x /usr/bin/entrypoint.sh
-
-USER app
-
-VOLUME $APPROOT/storage
+VOLUME ["$APPROOT/tmp", "$APPROOT/log", "$APPROOT/storage"]
 
 ENTRYPOINT ["/usr/bin/entrypoint.sh"]
 CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
--- containers/entrypoint.sh
+++ containers/entrypoint.sh
@@ -5,8 +5,44 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# Set defaults
+[ -z "$RAILS_ENV" ] && export RAILS_ENV=production
+[ -z "$RACK_ENV" ] && export RACK_ENV=$RAILS_ENV
+[ -z "$JAVASCRIPT_DRIVER" ] && export JAVASCRIPT_DRIVER=headless_chrome
+[ -z "$SPEC_USER_TIME_ZONE" ] && export SPEC_USER_TIME_ZONE=Etc/UTC
+[ -z "$DEFAULT_SENDER_EMAIL_ADDRESS" ] && export DEFAULT_SENDER_EMAIL_ADDRESS=admin@$ASSET_HOST
+[ -z "$SSO_DOMAIN" ] && export SSO_DOMAIN=sso.$ASSET_HOST
+[ -z "$CAPYBARA_MAX_WAIT_TIME" ] && export CAPYBARA_MAX_WAIT_TIME=30
+[ -z "$SLOWPOKE_TIMEOUT" ] && export SLOWPOKE_TIMEOUT=300
+[ -z "$WEBHOOK_READ_TIMEOUT" ] && export WEBHOOK_READ_TIMEOUT=30
+[ -z "$GRAPH_API_RATE_LIMIT" ] && export GRAPH_API_RATE_LIMIT=30
+[ -z "$RUBYOPT" ] && export RUBYOPT="-W:no-deprecated -W:no-experimental"
+[ -z "$RAILS_SERVE_STATIC_FILES" ] && export RAILS_SERVE_STATIC_FILES=true
+
+# This has been set in container metadata, so let's keep it hardcoded
+export PORT=3000
+
 # Prepare DB
-bundle exec rails db:setup
+err=0
+[ -z "DB_HOST" ] && "DB_HOST is mandatory" && err=1
+[ -z "DB_NAME" ] && "DB_NAME is mandatory" && err=1
+[ -z "DB_USERNAME" ] && "DB_USERNAME is mandatory" && err=1
+[ -z "DB_PASSWORD" ] && "DB_PASSWORD is mandatory" && err=1
+[ $err -gt 0 ] && exit 1
+
+bundle exec rails db:setup || :
+
+# Bootstrap the App
+err=0
+[ -z "$INITIAL_ADMIN_PASSWORD" ] && echo "INITIAL_ADMIN_PASSWORD is mandatory" && err=1
+[ -z "$ASSET_HOST" ] && echo "ASSET_HOST is mandatory" && err=1
+[ $err -gt 0 ] && exit 1
+
+bundle exec rails console -e $RAILS_ENV <<.
+user = User.find_by(email: 'admin@example.com')
+user.update!(password: '$INITIAL_ADMIN_PASSWORD', password_confirmation: '$INITIAL_ADMIN_PASSWORD')
+School.first.domains.create!(fqdn: '$ASSET_HOST', primary: true)
+.
 
 # Run container
 exec "$@"
